1b48276d95b265ada1eb85fda2ef353528aba6f7
Ryan Sheikholeslami <197540961+rshei222@users.noreply.github.com>
2026-09-12 14:22:05 -0400

    Create functions getHumanChoice(), playRound()
    Function getHumanChoice takes rps input from user, function playRound
    simulates a game with user input and computer input.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+//global scope
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
 function getComputerChoice() {
     let num = Math.floor( ((Math.random() * 3) + 1) ); //rand int from 1 to 3
     if (num == 1) {
@@ -11,4 +21,38 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+    choice = prompt("rock, paper, or scissors: ");
+    return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" & computerChoice == "scissors") {
+        humanScore++;
+        console.log("You win! Rock beats scissors");
+    }
+    else if (humanChoice == "scissors" & computerChoice == "rock") {
+        computerScore++;
+        console.log("You lose. Rock beats scissors");
+    }
+    else if (humanChoice == "paper" & computerChoice == "rock") {
+        humanScore++;
+        console.log("You win! Paper beats rock");
+    }
+    else if (humanChoice == "rock" & computerChoice == "paper") {
+        computerScore++;
+        console.log("You lose. Paper beats rock");
+    }
+    else if (humanChoice == "scissors" & computerChoice == "paper") {
+        humanScore++;
+        console.log("You win! Scissors beats paper");
+    }
+    else if (humanChoice == "paper" & computerChoice == "scissors") {
+        computerScore++;
+        console.log("You lose. Scissors beats paper");
+    }
+    else if (humanChoice == computerChoice) {
+        console.log("Tie. You both selected the same choice");
+    }
+}
+
